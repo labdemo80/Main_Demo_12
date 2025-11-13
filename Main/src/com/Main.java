8fb80a2d2b1f09559d5f0076fcdf1b04e55f89ac
labdemo80 <labdemo80@gmail.com>
2025-11-13 10:13:22 +0530

    New changes

--- Main/src/com/Main.java
+++ Main/src/com/Main.java
@@ -1,7 +1,16 @@
 package com;
 
+class Emp{
+	int id;
+	String name;
+	void display() {
+		System.out.println(id+" "+name);
+	}
+}
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("Test12");
 	}
 }
